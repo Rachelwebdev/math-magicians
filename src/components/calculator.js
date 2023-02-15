c74559e106de4dda4bc9938c4139cad4b6429fd8
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-15 13:18:48 +0400

    Implement Event listeners to the buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,32 +1,158 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
+  const operateEvent = (event) => {
+    const buttonName = event.target.innerHTML;
+    const result = calculate(state, buttonName);
+    setState(result);
+  };
+  const { total, operation, next } = state;
   return (
     <>
       <div className="calcContainer">
-        <div id="resultDisplay">0</div>
+        <div id="resultDisplay">
+          {total}
+          {operation}
+          {next}
+        </div>
         <div className="buttons">
-          <div className="btn">AC</div>
-          <div className="btn">+/-</div>
-          <div className="btn">%</div>
-          <div className="btn operation">÷</div>
-          <div className="btn">7</div>
-          <div className="btn">8</div>
-          <div className="btn">9</div>
-          <div className="btn operation">x</div>
-          <div className="btn">4</div>
-          <div className="btn">5</div>
-          <div className="btn">6</div>
-          <div className="btn operation">-</div>
-          <div className="btn">1</div>
-          <div className="btn">2</div>
-          <div className="btn">3</div>
-          <div className="btn operation">+</div>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            AC
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            +/-
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            %
+          </button>
+          <button
+            type="button"
+            className="btn operation"
+            onClick={operateEvent}
+          >
+            ÷
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            7
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            8
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            9
+          </button>
+          <button
+            type="button"
+            className="btn operation"
+            onClick={operateEvent}
+          >
+            x
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            4
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            5
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            6
+          </button>
+          <button
+            type="button"
+            className="btn operation"
+            onClick={operateEvent}
+          >
+            -
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            1
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            2
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            3
+          </button>
+          <button
+            type="button"
+            className="btn operation"
+            onClick={operateEvent}
+          >
+            +
+          </button>
         </div>
         <div className="lastRow">
-          <div className="btn">0</div>
-          <div className="btn">.</div>
-          <div className="btn operation">=</div>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            0
+          </button>
+          <button
+            type="button"
+            className="btn"
+            onClick={operateEvent}
+          >
+            .
+          </button>
+          <button
+            type="button"
+            className="btn operation"
+            onClick={operateEvent}
+          >
+            =
+          </button>
         </div>
       </div>
     </>
