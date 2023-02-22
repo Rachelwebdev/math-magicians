dc8a3bca2a12c900be2c4da4d52c3ad068109fec
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-22 13:52:30 +0400

    Add styles to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,47 @@
 import React from 'react';
+import styles from '../styles/Home.module.css';
 
 function Home() {
   return (
     <>
-      <h1>Welcome to our page!</h1>
-      <p className="paragrapgh">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia labore
-        provident quod blanditiis nisi. Repudiandae unde neque quisquam dicta
-        pariatur rerum temporibus minima sapiente ullam? Sed facilis possimus at
-        eligendi! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        <br />
-        Incidunt laudantium cum esse, unde perferendis deserunt magni cupiditate
-        nihil reiciendis soluta nesciunt reprehenderit, repellendus ipsum autem
-        sint harum dignissimos fugit sequi.
-        <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-        rerum, earum, pariatur possimus quaerat quidem nesciunt nisi, tenetur
-        sint optio neque exercitationem dolore nam quibusdam perspiciatis quasi
-        beatae cum est?
-      </p>
+      <div className={styles.homeContainer}>
+        <h1 className={styles.heading}>Welcome to our page!</h1>
+        <p className={styles.paragrapgh}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia labore
+          provident quod blanditiis nisi. Repudiandae unde neque quisquam dicta
+          pariatur rerum temporibus minima sapiente ullam? Sed facilis possimus
+          at eligendi! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore saepe
+          modi nam. Nihil tempora asperiores odio quibusdam, hic cum laudantium
+          perferendis, nemo minima quia commodi! Ullam aut in laudantium
+          commodi!
+          <br />
+          <br />
+          Incidunt laudantium cum esse, unde perferendis deserunt magni
+          cupiditate nihil reiciendis soluta nesciunt reprehenderit, repellendus
+          ipsum autem sint harum dignissimos fugit sequi. Lorem, ipsum dolor sit
+          amet consectetur adipisicing elit. Mollitia voluptatibus, veniam
+          adipisci sed incidunt natus corrupti et aliquid ea facilis non quasi
+          suscipit blanditiis tempore accusantium delectus nam laudantium minus?
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat,
+          ducimus! At, ea rem! Ratione qui voluptate corporis aliquid sunt vel
+          facilis maiores, quod eligendi quasi iste adipisci quo reiciendis
+          placeat.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          rerum, earum, pariatur possimus quaerat quidem nesciunt nisi, tenetur
+          sint optio neque exercitationem dolore nam quibusdam perspiciatis
+          quasi beatae cum est? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quos nesciunt ut officiis unde expedita quisquam,
+          quasi soluta corporis rerum reiciendis assumenda architecto numquam
+          beatae consequuntur obcaecati libero quas laudantium maiores! Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Totam eum ipsam
+          inventore repellendus alias, perspiciatis magni fugiat quos atque quae
+          saepe ea perferendis praesentium ad quas dolorum distinctio id
+          exercitationem.
+        </p>
+      </div>
     </>
   );
 }
