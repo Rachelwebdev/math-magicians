a3b44dd4b7ce70a9dd56d93e4eeff19d9e3bfbff
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-22 07:08:40 +0400

    Add Quote page component

---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <>
+      <p className="quoteText">
+        Mathematics is not about numbers, equations, computations, or
+        algorithms: it is about understanding. -William Paul Thurston
+      </p>
+    </>
+  );
+}
+
+export default Quote;
