7650e514db18a126595500606eeff03e4a4a9420
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-22 14:53:44 +0400

    Edit Calculator story and media query

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,39 +7,33 @@ function Home() {
       <div className={styles.homeContainer}>
         <h1 className={styles.heading}>Welcome to our page!</h1>
         <p className={styles.paragrapgh}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia labore
-          provident quod blanditiis nisi. Repudiandae unde neque quisquam dicta
-          pariatur rerum temporibus minima sapiente ullam? Sed facilis possimus
-          at eligendi! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore saepe
-          modi nam. Nihil tempora asperiores odio quibusdam, hic cum laudantium
-          perferendis, nemo minima quia commodi! Ullam aut in laudantium
-          commodi!
-          <br />
-          <br />
-          Incidunt laudantium cum esse, unde perferendis deserunt magni
-          cupiditate nihil reiciendis soluta nesciunt reprehenderit, repellendus
-          ipsum autem sint harum dignissimos fugit sequi. Lorem, ipsum dolor sit
-          amet consectetur adipisicing elit. Mollitia voluptatibus, veniam
-          adipisci sed incidunt natus corrupti et aliquid ea facilis non quasi
-          suscipit blanditiis tempore accusantium delectus nam laudantium minus?
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat,
-          ducimus! At, ea rem! Ratione qui voluptate corporis aliquid sunt vel
-          facilis maiores, quod eligendi quasi iste adipisci quo reiciendis
-          placeat.
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          rerum, earum, pariatur possimus quaerat quidem nesciunt nisi, tenetur
-          sint optio neque exercitationem dolore nam quibusdam perspiciatis
-          quasi beatae cum est? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Quos nesciunt ut officiis unde expedita quisquam,
-          quasi soluta corporis rerum reiciendis assumenda architecto numquam
-          beatae consequuntur obcaecati libero quas laudantium maiores! Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Totam eum ipsam
-          inventore repellendus alias, perspiciatis magni fugiat quos atque quae
-          saepe ea perferendis praesentium ad quas dolorum distinctio id
-          exercitationem.
+          Once upon a time, there was a young programmer named Rachel who was
+          tasked with building a math calculator using React. Rachel had never
+          built a calculator before, so she was nervous but excited to take on
+          the challenge. As she began working on the project, Rachel quickly
+          realized that building a calculator was not as simple as she had
+          initially thought. She struggled to figure out how to get the buttons
+          to display the correct numbers and how to perform the right
+          operations. But Rachel was determined to figure it out. She spent
+          hours poring over documentation, reading tutorials, and experimenting
+          with different approaches. She tried using different libraries and
+          tools, but nothing seemed to work quite right. Just when Rachel was
+          about to give up, she had an idea. She remembered reading a story
+          about a mathematician who used a unique method to solve a particularly
+          tricky problem. She decided to try applying that same method to her
+          calculator. Rachel spent the next few days working tirelessly on her
+          calculator, using the mathematicians method to guide her approach.
+          Slowly but surely, she began to see progress. The numbers and
+          operations were finally working correctly, and the calculator was
+          starting to take shape. As she neared the end of the project, Rachel
+          felt a sense of pride and accomplishment. She had tackled a difficult
+          challenge and come out the other side victorious. And in the process,
+          she had discovered a new way of approaching programming problems that
+          she could use in the future. In the end, her math calculator was a
+          thing of beauty, with a sleek design and intuitive interface. It was a
+          testament to her hard work, perseverance, and creativity. And whenever
+          she looked at it, she felt a sense of satisfaction knowing that she
+          had built something truly amazing.
         </p>
       </div>
     </>
