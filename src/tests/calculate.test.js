aaf405d4a717b419dd1fc50f5164250955d96e8d
Anthony Martin <anthonymartin20@gmail.com>
2023-02-22 15:09:05 +0100

    Testing calculate.js

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -99,4 +99,68 @@ describe('Unit testing for calculate.js', () => {
             operation: null,
         });
     });
+
+    test('When AC key is pressed screen must be empty', () => {
+        const myMockedObject = {
+            total: '100',
+            next: '0',
+            operation: '+',
+        };
+        
+        let MockedButtonName = 'AC';
+
+        expect(calculate(myMockedObject, MockedButtonName)).toStrictEqual({
+            total: null,
+            next: null,
+            operation: null,
+        });
+    });
+
+    test('+/- key should change 100 to -100', () => {
+        const myMockedObject = {
+            total: null,
+            next: '100',
+            operation: null,
+        };
+        
+        let MockedButtonName = '+/-';
+
+        expect(calculate(myMockedObject, MockedButtonName)).toStrictEqual({
+            total: null,
+            next: '-100',
+            operation: null,
+        });
+    });
+
+    test('+/- key should change -100 to 100', () => {
+        const myMockedObject = {
+            total: null,
+            next: '-100',
+            operation: null,
+        };
+        
+        let MockedButtonName = '+/-';
+
+        expect(calculate(myMockedObject, MockedButtonName)).toStrictEqual({
+            total: null,
+            next: '100',
+            operation: null,
+        });
+    });
+
+    test('10 % 100 should be 10', () => {
+        const myMockedObject = {
+            total: '10',
+            next: '100',
+            operation: '%',
+        };
+        
+        let MockedButtonName = '=';
+
+        expect(calculate(myMockedObject, MockedButtonName)).toStrictEqual({
+            total: '10',
+            next: null,
+            operation: null,
+        });
+    });
 });
