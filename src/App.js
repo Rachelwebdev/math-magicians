f310cbf00113d342ccb68c067d1fc6af54e4f149
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-22 06:38:13 +0400

    Add Navbar component and render it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Calculator from './components/calculator';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Calculator />
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav>
+    <h1 className="logo">Math Magician</h1>
+    <div className="navLinks">
+      <Link>Home</Link>
+      <Link>Calculator</Link>
+      <Link>Quote</Link>
+    </div>
+  </nav>
+);
+
+export default Navbar;
