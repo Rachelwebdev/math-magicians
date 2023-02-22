9b2a9fd0de7cbb9651918d7d9ddf573bb1fae26e
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-22 07:08:19 +0400

    Add home page component

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <h1>Welcome to our page!</h1>
+      <p className="paragrapgh">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia labore
+        provident quod blanditiis nisi. Repudiandae unde neque quisquam dicta
+        pariatur rerum temporibus minima sapiente ullam? Sed facilis possimus at
+        eligendi! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        <br />
+        Incidunt laudantium cum esse, unde perferendis deserunt magni cupiditate
+        nihil reiciendis soluta nesciunt reprehenderit, repellendus ipsum autem
+        sint harum dignissimos fugit sequi.
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        rerum, earum, pariatur possimus quaerat quidem nesciunt nisi, tenetur
+        sint optio neque exercitationem dolore nam quibusdam perspiciatis quasi
+        beatae cum est?
+      </p>
+    </>
+  );
+}
+
+export default Home;
