3b7811102c3cf02877eb37c61262a2d22bfacb64
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-22 13:25:38 +0400

    Add routes to each page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/calculator';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Quote from './components/Quote';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-      <Calculator />
-    </div>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/calculator"
+          element={<Calculator />}
+        />
+        <Route
+          path="/quote"
+          element={<Quote />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </>
   );
 }
 
